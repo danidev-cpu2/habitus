c3452dbf4594296b75112ffbbfcc042e18012cdb
Danidev <danidev@MacBook-Pro-de-Danidev.local>
2026-05-07 18:04:41 +0200

    Añadiendo el contador de psicologos activos

--- habitus/src/app/pages/habitus.component/admin.component/admin.component.ts
+++ habitus/src/app/pages/habitus.component/admin.component/admin.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, signal } from '@angular/core';
+import { UserService } from '../../../core/services/user.service';
+import { User } from '../../../core/models/user.model';
 
 @Component({
   selector: 'app-admin.component',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css',
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit {
+  readonly activePsychologistsCount = signal<number | null>(null);
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getByRole('psychologist').subscribe({
+      next: (data: User[]) => {
+        this.activePsychologistsCount.set(
+          (data ?? []).filter((p) => p.status === 'active').length
+        );
+      },
+      error: () => {
+        this.activePsychologistsCount.set(null);
+      },
+    });
+  }
 }
