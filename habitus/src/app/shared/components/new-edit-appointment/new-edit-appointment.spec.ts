6ca562324efba43c2308525a0029c41beef68be6
taylor <taylorher0601@gmail.com>
2026-05-15 15:10:09 +0200

    Cambio menor

--- habitus/src/app/shared/components/new-edit-appointment/new-edit-appointment.spec.ts
+++ habitus/src/app/shared/components/new-edit-appointment/new-edit-appointment.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NewEditAppointment } from './new-edit-appointment';
+import { NewEditAppointment } from './new-edit-appointment.component';
 
 describe('NewEditAppointment', () => {
   let component: NewEditAppointment;
