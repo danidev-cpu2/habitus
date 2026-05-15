cd986bcc4c7c2bb0a0ed73e342a973ab1d3370e5
Danidev <danidev@MacBook-Pro-de-Danidev.local>
2026-05-15 16:40:32 +0200

    mergealoya

--- habitus/src/app/pages/habitus.component/patient.component/index-pax.component/index-pax.component.ts
+++ habitus/src/app/pages/habitus.component/patient.component/index-pax.component/index-pax.component.ts
@@ -23,12 +23,7 @@ export interface AppointmentView {
 
 @Component({
   selector: 'app-index-pax',
-<<<<<<< HEAD
   imports: [CommonModule],
-=======
-  standalone: true,
-  imports: [],
->>>>>>> origin
   templateUrl: './index-pax.component.html',
 })
 export class IndexPaxComponent implements OnInit {
