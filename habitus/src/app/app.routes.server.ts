91d19701cbaab08a634308ec390e8119e9d23ddb
Danidev <danidev@MacBook-Pro-de-Danidev.local>
2026-05-23 01:56:54 +0200

    Configure GitHub Pages deployment for Angular

--- habitus/src/app/app.routes.server.ts
+++ habitus/src/app/app.routes.server.ts
@@ -2,7 +2,19 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'admin/usuarios/:id/editar',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'psychologist/pacientes/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'psicologo/pacientes/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
